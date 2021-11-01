da7b61d9402c3fde538f9a19e91a252fa134368d
anatolytsi <snipl33t@gmail.com>
2021-11-01 19:27:10 +0100

    updated case loading sequence

--- backend/python/my_room.py
+++ backend/python/my_room.py
@@ -1,41 +1,50 @@
 import time
 
 from backend.python.wopsimulator.objects.heater import WopHeater
-from backend.python.wopsimulator.loader import create_case, load_case
+from backend.python.wopsimulator.loader import create_case, load_case, save_case
 
 
 def main():
     case_name = 'my_room'
-    is_run_parallel = True
     timestep = 30
+    case_parameters = {
+        "type": "cht_room",
+        "path": "./my_room.case",
+        "blocking": False,
+        "parallel": True,
+        "cores": 4,
+        "background": "fluid"
+    }
     try:
-        case_cls, case_path = create_case(case_name, 'cht_room')
-        room = case_cls(case_name, is_blocking=False, is_run_parallel=is_run_parallel, num_of_cores=4)
-        # Here follows a desired future behavior after creation of a new case
-        # room.create(dimensions=[2, 3, 4])
-        # # or
-        # room.create(stl_path='./room.stl')
-        #
-        # room.add_heater(name='heater1', location=[1.5, 0.5, 0], heater_template='convective1')
-        # room.add_heater(name='heater2', location=[0, 0, 0], dimensions=[0.1, 0.2, 0.3], heater_type='convective',...)
-        # room.add_window(dimensions=[1, 1], location=[0, 1.5, 2])
-        # room.add_door(dimensions=[1, 2], location=[1.5, 0, 2])
+        room = create_case(case_name, case_parameters)
+        room_dimensions = [3, 4, 2.5]
+        window_dimension = [1.5, 0, 1.25]
+        door_dimension = [1.5, 0, 2]
+        heater_dimensions = [1, 0.2, 0.7]
+
+        window_location = [(room_dimensions[0] - window_dimension[0]) / 2,
+                           0,
+                           (room_dimensions[2] - window_dimension[2]) / 2]
+        door_location = [(room_dimensions[0] - door_dimension[0]) / 2,
+                         room_dimensions[1],
+                         (room_dimensions[2] - door_dimension[2]) / 2]
+        heater_location = [1, 1, 0.2]
+        sensor_location = [1.5, 2, 1]
+        room.add_object(name='walls', obj_type='walls', dimensions=room_dimensions)
+        room.add_object('inlet', 'window', dimensions=window_dimension, location=window_location)
+        room.add_object('outlet', 'door', dimensions=door_dimension, location=door_location)
+        room.add_object('heater', 'heater', dimensions=heater_dimensions, location=heater_location)
+        room.add_object('temp_sensor', 'sensor', location=sensor_location, sns_field='T')
+        room.setup()
+        room.heaters['heater'].temperature = 450
+        save_case(case_name, room)
 
     except FileExistsError:
-        case_cls, case_path = load_case(case_name)
-        room = case_cls(case_name, is_blocking=False, is_run_parallel=is_run_parallel, num_of_cores=4)
-    room.setup()
+        room = load_case(case_name)
+
     room.run()
     time.sleep(timestep)
     room.stop()
-    # room['heater1'].turn_on(50)  # TODO: This is a desired future implementation
-    # room.run()
-    # time.sleep(timestep)
-    # room.stop()
-    # room['heater1'].turn_off  # TODO: This is a desired future implementation
-    # room.run()
-    # time.sleep(timestep)
-    # room.stop()
 
 
 if __name__ == '__main__':
