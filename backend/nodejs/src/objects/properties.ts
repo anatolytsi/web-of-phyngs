81feb9542adf44db2de1c65c99f686698adc7c82
anatolytsi <snipl33t@gmail.com>
2021-11-28 19:49:22 +0100

    corrected property requests

--- backend/nodejs/src/objects/properties.ts
+++ backend/nodejs/src/objects/properties.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export class HeatingProperties {
     public async getTemperature(url: string) {
-        let response = await axios.get(`${url}/temperature/value`);
+        let response = await axios.get(`${url}/temperature`);
         return response.data;
     }
 
     public async setTemperature(url: string, temperature: number) {
-        await axios.post(`${url}/temperature/`, {value: temperature});
+        await axios.post(`${url}/temperature`, {value: temperature});
     }
 
     protected setTemperatureGetHandler(thing: WoT.ExposedThing, url: string) {
@@ -17,7 +17,7 @@ export class HeatingProperties {
     }
 
     protected setTemperatureSetHandler(thing: WoT.ExposedThing, url: string) {
-        thing.setActionHandler('setTemperature', async (temperature) => {
+        thing.setPropertyWriteHandler('temperature', async (temperature) => {
             await this.setTemperature(url, temperature);
         });
     }
@@ -25,12 +25,12 @@ export class HeatingProperties {
 
 export class VelocityProperties {
     public async getVelocity(url: string) {
-        let response = await axios.get(`${url}/velocity/value`);
+        let response = await axios.get(`${url}/velocity`);
         return response.data;
     }
 
     public async setVelocity(url: string, velocity: Array<number>) {
-        await axios.post(`${url}/velocity/`, {value: velocity});
+        await axios.post(`${url}/velocity`, {value: velocity});
     }
 
     protected setVelocityGetHandler(thing: WoT.ExposedThing, url: string) {
@@ -40,7 +40,7 @@ export class VelocityProperties {
     }
 
     protected setVelocitySetHandler(thing: WoT.ExposedThing, url: string) {
-        thing.setActionHandler('setVelocity', async (velocity) => {
+        thing.setPropertyWriteHandler('velocity', async (velocity) => {
             await this.setVelocity(url, velocity);
         });
     }
@@ -48,23 +48,29 @@ export class VelocityProperties {
 
 export class OpenableProperties {
     public async getOpen(url: string) {
-        let response = await axios.get(`${url}/open/value`);
+        let response = await axios.get(`${url}/open`);
         return response.data;
     }
 
     public async setOpen(url: string, open: boolean) {
-        await axios.post(`${url}/open/`, {value: open});
+        await axios.post(`${url}/open`, {value: open});
     }
 
     protected setOpenGetHandler(thing: WoT.ExposedThing, url: string) {
-        thing.setPropertyReadHandler('open', async () => {
+        thing.setPropertyReadHandler('opened', async () => {
             return await this.getOpen(url);
         });
     }
 
     protected setOpenSetHandler(thing: WoT.ExposedThing, url: string) {
-        thing.setActionHandler('setOpen', async (open) => {
-            await this.setOpen(url, open);
+        thing.setActionHandler('open', async () => {
+            await this.setOpen(url, true);
+        });
+    }
+
+    protected setCloseSetHandler(thing: WoT.ExposedThing, url: string) {
+        thing.setActionHandler('close', async () => {
+            await this.setOpen(url, false);
         });
     }
 }
