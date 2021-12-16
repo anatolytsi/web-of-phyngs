8d24a072b05c35a492b67bd53a5210ed17d52bda
anatolytsi <snipl33t@gmail.com>
2021-12-16 18:59:39 +0100

    removed unnecessary functionality and added host address from environment

--- backend/nodejs/src/index.ts
+++ backend/nodejs/src/index.ts
@@ -4,19 +4,6 @@ import {HttpServer} from '@node-wot/binding-http';
 import {CaseConstructorType, Simulator} from './base/simulator';
 import {ChtCase} from './behavior/cht';
 
-const {spawn} = require('child_process');
-
-const delay = (ms: number) => new Promise(resolve => {
-    setTimeout(() => {
-        resolve(2);
-    }, ms);
-});
-
-let SIMULATION_HOST: string = 'http://127.0.0.1:5000';
-let simulationHostAddressResolver: Function;
-let simulationHostAddress = new Promise(resolve => {
-    simulationHostAddressResolver = resolve;
-})
 let CASE_PARAMS: CaseConstructorType = {
     'cht': {
         'constructor': (host: string, wot: WoT.WoT,
@@ -25,26 +12,12 @@ let CASE_PARAMS: CaseConstructorType = {
     }
 }
 
-// const pythonSimulator = spawn('python3', [`${__dirname}/../../python/server.py`]);
-// pythonSimulator.stdout.on('data', (data: any) => {
-//     console.log(`${data}`);
-// });
-//
-// pythonSimulator.stderr.on('data', (data: any) => {
-//     let match = data.toString().match('.Running on ([^\\s]*)\\s');
-//     if (match) {
-//         SIMULATION_HOST = match[1].substring(0, match[1].length - 1);
-//         simulationHostAddressResolver(true);
-//     }
-//     console.error(`${data}`);
-// });
-
 let httpServer = new HttpServer({port: 8080});
 let servient = new Servient();
 servient.addServer(httpServer);
 
 servient.start()
     .then(async (WoT) => {
-        // await simulationHostAddress;
-        let simulator = new Simulator(SIMULATION_HOST, WoT, require('../tms/simulator.model.json'), CASE_PARAMS);
+        let pythonHost: string = process.env.PYTHON_SERVER || 'http://127.0.0.1:5000';
+        let simulator = new Simulator(pythonHost, WoT, require('../tms/simulator.model.json'), CASE_PARAMS);
     })
