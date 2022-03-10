f850830a7f0921e30d573f3fb3c8ee3046e0a22d
ge75tin <anatolii.tsirkunenko@tum.de>
2022-03-10 13:55:36 +0100

    pvserver: added multicore

--- backend/paraview/start.sh
+++ backend/paraview/start.sh
@@ -2,6 +2,8 @@
 
 while true; do
 
-    /home/paraview/package/bin/pvserver --multi-clients --force-offscreen-rendering && wait
+    /home/paraview/package/bin/mpiexec -np $CORES \
+    /home/paraview/package/bin/pvserver --multi-clients --force-offscreen-rendering \
+    && wait
 
 done
