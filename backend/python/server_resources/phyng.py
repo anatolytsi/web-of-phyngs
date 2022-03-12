399705214c74e271ebbe08c433693a060e2bce9b
ge75tin <anatolii.tsirkunenko@tum.de>
2022-03-12 22:47:54 +0100

    flask: added deserialization of phyng values

--- backend/python/server_resources/phyng.py
+++ backend/python/server_resources/phyng.py
@@ -1,3 +1,5 @@
+import json
+
 import werkzeug.datastructures
 from flask_restful import Resource, reqparse
 
@@ -111,7 +113,7 @@ class PhyngValue(Resource):
         value = self.reqparse.parse_args(strict=True)['value']
         phyng = self.current_cases[case_name].get_phyng(phyng_name)
         if phyng_value in phyng:
-            phyng[phyng_value] = value
+            phyng[phyng_value] = json.loads(value)
             return '', 200
         # TODO: move this error
         raise KeyError(f'Property "{phyng_value}" for phyng "{phyng_name} does not exist')
